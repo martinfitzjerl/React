3b740e2d399bac4ee38f8ca037d601eb4000eadf
Samuel Baker <Samuel.Baker9@outlook.com>
2018-12-25 20:17:04 -0500

    modified App.js

--- material-login/src/App.js
+++ material-login/src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import logo from './investorCointwo.svg';
+import { withStyles } from '@material-ui/core/styles';
 import './App.css';
 import InputField from './InputField';
 import Avatar from './Avatar';
 import AppBar from './AppBar';
+import Fab from '@material-ui/core/Fab';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
+const styles = theme => ({
+  fab: {
+    margin: theme.spacing.unit,
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
 
 class App extends Component {
   render() {
@@ -27,10 +38,22 @@ class App extends Component {
           </a>
         </header>
         <InputField/>
+        <form>
+        <div>
+      <Fab variant="extended" aria-label="Login" color="primary" >
+        <AccountBoxIcon   />
+        LOGIN
+        
+      </Fab>
+
+    </div>
+
+        </form>
       </div>
 
     );
   }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
